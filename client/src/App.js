254924339773c0ae6770054b062abc5f3aca1698
Cherrypick14 <cherrypicklet@gmail.com>
2023-02-17 00:39:03 +0300

    add layout

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,40 @@ import {
 import Home from './pages/Home/home';
 import Products from './pages/Products/products';
 import Product from './pages/Product/product';
+import Navbar from './components/Navbar/navbar';
+import Footer from './components/Footer/footer';
+
+const Layout = ()=>{
+
+  return(
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>
+    element: <Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/products/:id",
+        element: <Products/>
+      },
+      {
+        path: "/product/:id",
+        element: <Product/>
+      },
+    ]
   },
-  {
-    path: "/products/:id",
-    element: <Products/>
-  },
-  {
-    path: "/product/:id",
-    element: <Product/>
-  },
+  
 ])
 function App() {
 
