5471aebda9fad65d19820b402d189e3edcc8ace2
Cherrypick14 <cherrypicklet@gmail.com>
2023-02-20 23:39:03 +0300

    add navbar layout

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -4,14 +4,66 @@ import SearchIcon from '@mui/icons-material/Search';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Link } from 'react-router-dom';
+import '../../styles/navbar.scss'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="wrapper">
-             <div className="left"></div>
-             <div className="center"></div>
-             <div className="right"></div>
+             <div className="left">
+              <div className="item">
+                 <img src="/img/en.png" alt="" />
+                  <KeyboardArrowDownIcon />
+              </div>
+                 <div className="item">
+                     <span>USD</span>
+                     <KeyboardArrowDownIcon />
+               </div>
+
+                 <div className="item">
+                   <Link to ="/products/1">Women</Link>
+                 </div>
+                 <div className="item">
+                   <Link to ="/products/2">Men</Link>
+                 </div>
+                 <div className="item">
+                   <Link to ="/products/3">Children</Link>
+                 </div>
+
+             </div>
+
+             <div className="center">
+                <Link to ="/">KIDDOSTORE</Link>
+             </div>
+
+             <div className="right">
+
+             <div className="item">
+                <Link to ="/">Home</Link>
+             </div>
+             <div className="item">
+                <Link to ="/">About</Link>
+             </div>
+             <div className="item">
+                <Link to ="/">Contact</Link>
+             </div>
+             <div className="item">
+                <Link to ="/">STORES</Link>
+             </div>
+                <div className='icons'>
+                   <SearchIcon />
+                   <PersonOutlineIcon />
+                   <FavoriteBorderOutlinedIcon />
+
+                   <div className='cartIcon'>
+                      <ShoppingCartOutlinedIcon />
+                      <span>0</span>
+                   </div>
+
+                </div>
+
+             </div>
         </div>
     </div>
   )
