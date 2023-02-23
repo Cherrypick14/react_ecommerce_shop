304bb0042687c2d122636901a43f742ff6320d6b
Cherrypick14 <cherrypicklet@gmail.com>
2023-02-23 21:51:24 +0300

    add new slider component

---
+++ client/src/components/Slider/slider.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../../styles/slider.scss"
+
+const Slider = () => {
+    
+  return (
+    <div className='slider'>
+        <div className="container">
+             <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Slider
