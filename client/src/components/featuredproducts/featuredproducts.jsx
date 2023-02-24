b90a65db0ae0ed97a4d8ebe6060b8f9ff0c6b55b
Cherrypick14 <cherrypicklet@gmail.com>
2023-02-24 22:50:56 +0300

    add featured products

---
+++ client/src/components/featuredproducts/featuredproducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../styles/featuredproducts.scss"
+
+const Featuredproducts = () => {
+  return (
+    <div className='featuredproducts'>
+        
+    </div>
+  )
+}
+
+export default Featuredproducts
