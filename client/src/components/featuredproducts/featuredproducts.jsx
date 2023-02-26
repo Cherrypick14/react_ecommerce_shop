2f10d7e16b37e21c7f5cf23e28e0216fb31247dd
Cherrypick14 <cherrypicklet@gmail.com>
2023-02-26 23:21:05 +0300

    add our props

--- client/src/components/featuredproducts/featuredproducts.jsx
+++ client/src/components/featuredproducts/featuredproducts.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import "../../styles/featuredproducts.scss"
 
-const Featuredproducts = () => {
+const Featuredproducts = ({type}) => {
   return (
     <div className='featuredproducts'>
          <div className="top">
-           <h1>products</h1>
+           <h1>{type}products</h1>
            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit quia, dicta eum ipsa non ullam consequatur harum quasi quis iusto asperiores consectetur dolorum incidunt recusandae molestias tempora esse? Repellat, temporibus.</p>
 
          </div>
