c79f0dacd2c4bb4e01193bb318c5fc9004ea62ce
Cherrypick14 <cherrypicklet@gmail.com>
2023-02-28 12:56:59 +0300

    modify our data array

--- client/src/components/featuredproducts/featuredproducts.jsx
+++ client/src/components/featuredproducts/featuredproducts.jsx
@@ -7,7 +7,8 @@ const Featuredproducts = ({type}) => {
   const data  = [
    {
     id:1,
-    img:"https://www.google.com/imgres?imgurl=https%3A%2F%2Ftheholidaydealers.com%2Fwp-content%2Fwebpc-passthru.php%3Fsrc%3Dhttps%3A%2F%2Ftheholidaydealers.com%2Fwp-content%2Fuploads%2F2019%2F01%2FMfangano-Island-Camp-10.jpg%26nocache%3D1&imgrefurl=https%3A%2F%2Ftheholidaydealers.com%2Fdestinations%2Fkenya%2Fkisumu%2Fmfangano-island-camp%2F&tbnid=kIorST8ly8lOLM&vet=12ahUKEwi3g8ztg7T9AhXvmicCHS78BHQQMygIegUIARD0AQ..i&docid=mWCctm5zpLDOFM&w=900&h=500&q=mfangano%20island&ved=2ahUKEwi3g8ztg7T9AhXvmicCHS78BHQQMygIegUIARD0AQ",
+    img:"./img/alexi-romano.jpg",
+    img2:"./img/mukuko-studio.jpg",
     title:'Long Sleeve Graphic Tee',
     isNew:true,
     oldPrice:19,
@@ -15,7 +16,7 @@ const Featuredproducts = ({type}) => {
    },
    {
     id:2,
-    img:"https://novanglobal.com/tour/mfangano-island-camp/",
+    img:"./img/atikh-bana.jpg",
     title:'Glovo',
     isNew:true,
     oldPrice:19,
@@ -23,7 +24,7 @@ const Featuredproducts = ({type}) => {
    },
    {
     id:3,
-    img:"https://unsplash.com/photos/78A265wPiO4",
+    img:"./img/dom-hill.jpg",
     title:'Go Pato',
     isNew:true,
     oldPrice:19,
@@ -31,7 +32,7 @@ const Featuredproducts = ({type}) => {
    },
    {
     id:4,
-    img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.booking.com%2Fhotel%2Fke%2Fmfangano-island-lodge.html&psig=AOvVaw0Q0GCPiNYp9hByR9gWX4ut&ust=1677533368309000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLDD_vaQtP0CFQAAAAAdAAAAABAE",
+    img:"./img/katsiaryna.jpg",
     title:'We good',
     isNew:true,
     oldPrice:19,
