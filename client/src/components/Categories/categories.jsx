b17d7267b2fdf378a0cbb5367e5ff28a45077969
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-01 18:44:48 +0300

    add new categ component

---
+++ client/src/components/Categories/categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../styles/categories.scss"
+
+const Categories = () => {
+  return (
+    <div className='categories'>
+
+    </div>
+  )
+}
+
+export default Categories
