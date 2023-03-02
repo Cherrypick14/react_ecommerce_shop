0ee648719b99dda410525af284596a12f10524df
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-02 23:48:21 +0300

    add our contact component

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Categories from '../../components/Categories/categories'
+import Contact from '../../components/Contact/contact'
 import Featuredproducts from '../../components/featuredproducts/featuredproducts'
 import Slider from '../../components/Slider/slider'
 import "../../styles/home.scss"
@@ -12,6 +13,7 @@ const Home = () => {
        <Featuredproducts  type="featured"/>
        <Categories />
        <Featuredproducts  type="trending"/>
+       <Contact />
     </div>
   )
 }
