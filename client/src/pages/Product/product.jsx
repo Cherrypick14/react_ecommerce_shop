055286b86dca865a7893cf27183c7c477d21a203
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-06 00:03:32 +0300

    left side done

--- client/src/pages/Product/product.jsx
+++ client/src/pages/Product/product.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../../styles/product.scss"
 
 const Product = () => {
+
+  const[selectedImg, setSelectedImg] = useState(0)
+
+  const images =[
+     "https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=400",
+     "https://images.pexels.com/photos/965324/pexels-photo-965324.jpeg?auto=compress&cs=tinysrgb&w=400"
+  ]
   return (
-    <div className='product'>product</div>
+    <div className='product'>
+       <div className="left">
+          <div className="images">
+              <img src={images[0]} alt="" onClick={e => setSelectedImg(0)}/>
+              <img src={images[1]} alt="" onClick={e => setSelectedImg(1)} />
+
+          </div>
+          <div className="mainImg">
+             <img src={images[selectedImg]} alt="" />
+
+          </div>
+       </div>
+       <div className="right"></div>
+    </div>
   )
 }
 
