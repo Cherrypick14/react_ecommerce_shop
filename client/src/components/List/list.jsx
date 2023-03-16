446e7794911673283636543bbab85b8c3e8dac12
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-16 23:40:17 +0300

    fix filter by price option

--- client/src/components/List/list.jsx
+++ client/src/components/List/list.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import "../../styles/list.scss"
 import Card from '../Card/card'
+import useFetch  from "../../hooks/useFetch";
 
-const List = () => {
+const List = ({catId,maxPrice,sort,subCat}) => {
      
-       
+  const{data, loading, error} = useFetch(`/products?populate=*&[filters][categories][id]=${catId}
+  ${subCat.map(
+
+    (item) =>`&[filters][sub_categories][id][$eq]=${item}`
+    
+    )}&[filters][price][$lte]${maxPrice}`);   
+  
   return (
     <div className='list'>
-        {/* {data.map((item)=> <Card item={item} key={item.id}/>)} */}
+        {error?"something's up"
+         :loading ? "loading":data.map((item)=> <Card item={item} key={item.id}/>)}
     </div>
   )
 }
