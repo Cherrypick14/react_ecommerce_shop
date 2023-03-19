bc3ace0733d195b29b0bcf3635248840f605299c
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-19 20:58:19 +0300

    add upload img url

--- client/src/pages/Product/product.jsx
+++ client/src/pages/Product/product.jsx
@@ -1,85 +1,91 @@
-import React, { useState } from 'react';
+import React,{useState} from 'react'
 import "../../styles/product.scss";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import BalanceIcon from '@mui/icons-material/Balance';
-
+import useFetch  from "../../hooks/useFetch";
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
-
-  const[selectedImg, setSelectedImg] = useState(0);
-
-  const[quantity, setQuantity] = useState(1);
-
-  const images =[
-     "https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=400",
-     "https://images.pexels.com/photos/965324/pexels-photo-965324.jpeg?auto=compress&cs=tinysrgb&w=400"
-  ]
+    const id = useParams().id
+    const[selectedImg, setSelectedImg] = useState("img");  // initialize selectedImg state variable
+    const[quantity, setQuantity] = useState(1);
+    const url = `/products/${id}?populate=*`;
+    const{data, loading, error} = useFetch(url) ;
   return (
-    <div className='product'>
-       <div className="left">
-          <div className="images">
-              <img src={images[0]} alt="" onClick={e => setSelectedImg(0)}/>
-              <img src={images[1]} alt="" onClick={e => setSelectedImg(1)} />
+    <div className ="product">
+       {loading?("loading"):
+       (<>
+        <div className="left">
+            <div className="images">
+                <div className="images">
+                     <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img?.data?.attributes?.url}
+                     alt=""
+                  onClick={() => setSelectedImg("img")} // update selectedImg when clicked
+                      />
 
-          </div>
-          <div className="mainImg">
-             <img src={images[selectedImg]} alt="" />
+                     <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img2?.data?.attributes?.url}
+                     alt=""
+                     onClick={() => setSelectedImg("img2")} // update selectedImg when clicked
+                      />
+                 </div>
+                <div className="mainImg">
+                 <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes[selectedImg]?.data?.attributes?.url}
+                alt=""
+               />
+                </div>
+            </div>
+        </div>
+        <div className="right">
+        <h1>{data?.attributes?.title}</h1>
+            <span className='price'>{data?.attributes?.price}</span>
+            <p>{data?.attributes?.desc}</p>
+              <div className="quantity">
+                <button onClick={()=>setQuantity((prev)=> (prev === 1 ? 1 : prev- 1))}>-</button>
 
-          </div>
-       </div>
-       <div className="right">
-          <h1>Hat</h1>
-          <span className='price'>$18.55</span>
-           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia similique asperiores a odio eveniet, adipisci quos rerum magni nihil sequi. At sed excepturi nulla impedit repellat autem. Facilis, soluta consequuntur.</p>
-          <div className="quantity">
-             <button onClick={()=>setQuantity((prev)=> (prev === 1 ? 1 : prev- 1))}>-</button>
+                   {quantity}
 
-                 {quantity}
+                <button onClick={()=>setQuantity((prev)=>prev + 1)}>+</button>
 
-             <button onClick={()=>setQuantity((prev)=>prev + 1)}>+</button>
+             </div>
 
-          </div>
-
-           <button className="add">
+             <button className="add">
             
-            <AddShoppingCartIcon /> ADD TO CART
-      
-           </button>
-     <div className="links">
-       <div className="item">
-          <FavoriteBorderIcon /> ADD TO WISHLIST
-       </div>
-       <div className="item">
-           <BalanceIcon /> COMPARE
-       </div>
-     </div>
+             <AddShoppingCartIcon /> ADD TO CART
 
-     <div className="info">
+             </button>
+             <div className="links">
+          <div className="item">
+             <FavoriteBorderIcon /> ADD TO WISHLIST
+          </div>
+          <div className="item">
+             <BalanceIcon /> COMPARE
+          </div>   
+      </div>
+      <div className="info">
        <span>Vendor: Calvin</span>
        <span>Product Type: Poll Neck</span>
        <span>Tag: Men, women, p-neck </span>
 
      </div>
-   <hr/>
+     <hr/>
      <div className="details" style={{marginTop:"30px"}}>
         <span>Description</span>
 
-        <hr/>
+             <hr/>
 
-        <span>Additional Info</span>
+             <span>Additional Info</span>
 
-        <hr/>
+               <hr/>
 
-        <span>FAQs</span>
+                <span>FAQs</span>
+          </div>
+        </div>
+       </>)
+       }
 
-
-     </div>
-
-
-       </div>
     </div>
   )
 }
 
-export default Product;
+export default Product
