087ac3ea5c4a7aa88222dd816b5f0e469dff388a
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-23 02:41:10 +0300

    change the data type for data

--- client/src/components/featuredproducts/featuredproducts.jsx
+++ client/src/components/featuredproducts/featuredproducts.jsx
@@ -3,7 +3,6 @@ import "../../styles/featuredproducts.scss";
 import Card from '../Card/card';
 
 
-
 const Featuredproducts = ({type}) => {
  const{data, loading, error} = useFetch(`/products?populate=*&[filters][type][$eq]=${type}`)
 
@@ -16,7 +15,7 @@ const Featuredproducts = ({type}) => {
          </div>
          <div className="bottom">
               {error? "Something's up!":
-              (loading? "loading" : data.map((item) => <Card item={item} key={item.id}/>
+              (loading? "loading" : data?.map((item) => <Card item={item} key={item.id}/>
                 ))}
          </div>
     </div>
