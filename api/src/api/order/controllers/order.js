d9d8aaf9dbdd724a10a21ddfc4135652341fcdc9
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-26 00:25:24 +0300

    complete checkout with stripe

--- api/src/api/order/controllers/order.js
+++ api/src/api/order/controllers/order.js
@@ -9,33 +9,41 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::order.order',({strapi})=>({
       async create(ctx){
-         const{email, products} = ctx.request.body;
+         const{products} = ctx.request.body;
          const lineItems = await Promise.all(  //we use promise all because we have more than one product
           products.map(async (item)=>{
             const item = await strapi
             .service('api::product.product'
-            .findOne(product.id));
+            .findOne(item.id));
             return {
-                
+                price_data:{
+                    currency:"usd",
+                    product_data:{
+                        name: item.title
+                    },
+                    unit_amount:item.price*100
+                },
+                 quantity: item.quantity
             }
           })
         
          );
          try {
             const session = await stripe.checkout.sessions.create({
-                line_items: [
-                  {
-                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    price: '{{PRICE_ID}}',
-                    quantity: 1,
-                  },
-                ],
+                line_items: lineItems,
                 mode: 'payment',
                 success_url: `${process.env.CLIENT_URL}?success=true`,
                 cancel_url: `${process.env.CLIENT_URL}/?success=false`,
+                shipping_address:{allowed_countries:['US','AUS','KE','UK']},
+                payment_method: ["card"],
               });
-            
 
+              await strapi
+              .service("api::order.order")
+              .create({ data: {  products, stripeId: session.id } });
+
+              return { stripeSession: session };
+            
          } catch (error) {
             ctx.response.status = 500;
              return error
