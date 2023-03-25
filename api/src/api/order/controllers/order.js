06b55a09d43bee2a9453ddd1fd2b212ff85b3d04
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-25 23:28:38 +0300

    create our cart controller

--- api/src/api/order/controllers/order.js
+++ api/src/api/order/controllers/order.js
@@ -7,4 +7,9 @@ const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order',);
+module.exports = createCoreController('api::order.order',({strapi})=>({
+      async create(ctx){
+        
+      }
+
+}));
