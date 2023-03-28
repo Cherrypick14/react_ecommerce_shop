2225b9bc8a5029d8bb28d5b89447788d43aca292
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-28 17:01:47 +0300

    redo the try catch statement for the sessioncreat

--- api/src/api/order/controllers/order.js
+++ api/src/api/order/controllers/order.js
@@ -9,45 +9,47 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::order.order',({strapi})=>({
       async create(ctx){
-         const{products} = ctx.request.body;
-         const lineItems = await Promise.all(  //we use promise all because we have more than one product
-          products.map(async (product)=>{
-            const item = await strapi
-            .service('api::product.product')
-            .findOne(product.id);
-            return {
-                price_data:{
-                    currency:"usd",
-                    product_data:{
-                        name: item.title
-                    },
-                    unit_amount:Math.round(item.price*100),
-                },
-                 quantity: item.quantity
-            }
-          })
-        
-         );
+         const{ products } = ctx.request.body;
+
          try {
-            const session = await stripe.checkout.sessions.create({
-                line_items: lineItems,
-                mode: 'payment',
-                success_url: `${process.env.CLIENT_URL}?success=true`,
-                cancel_url: `${process.env.CLIENT_URL}/?success=false`,
-                shipping_address:{allowed_countries:['US','AUS','KE','UK']},
-                payment_method: ["card"],
-              });
+            const lineItems = await Promise.all(  //we use promise all because we have more than one product
+            products.map(async (product)=>{
+              const item = await strapi
+              .service('api::product.product')
+              .findOne(product.id);
+              return {
+                  price_data:{
+                      currency:"usd",
+                      product_data:{
+                          name: item.title
+                      },
+                      unit_amount:Math.round(item.price*100),
+                  },
+                   quantity: item.quantity,
+              }
+            })
+          
+           );
 
-              await strapi
-              .service("api::order.order")
-              .create({ data: {  products, stripeId: session.id } });
+         const session = await stripe.checkout.sessions.create({
+            shipping_address:{allowed_countries:['US','AUS','KE','UK']},
+            payment_method: ["card"],
+            mode: 'payment',
+            success_url: `${process.env.CLIENT_URL}?success=true`,
+            cancel_url: `${process.env.CLIENT_URL}/?success=false`,
+            line_items: lineItems,  
+          });
 
-              return { stripeSession: session };
-            
+          await strapi
+          .service("api::order.order")
+          .create({ data: {  products, stripeId: session.id } });
+
+          return { stripeSession: session };
          } catch (error) {
             ctx.response.status = 500;
-             return error
+            return { error };
          }
-      }
+          
+      },
 
 }));
