909fedb799fc36c2eb30cad461f9ec30ff6557c0
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-28 17:07:50 +0300

    add our sass file

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import {store,persistor} from './redux/store'
 import {Provider} from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
+import '../src/styles/app.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
