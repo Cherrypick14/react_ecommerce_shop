639f47bc4da252b529099789e37074343771a15f
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-28 17:07:18 +0300

    refactor some code

--- client/src/components/Cart/cart.jsx
+++ client/src/components/Cart/cart.jsx
@@ -2,61 +2,58 @@ import React from 'react'
 import "../../styles/cart.scss"
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import {useSelector,useDispatch} from "react-redux";
-import useFetch  from "../../hooks/useFetch";
-import { useParams } from 'react-router-dom';
-import {removeItem,resetCart} from '../../redux/cartReducer'
-import {makeRequest} from "../../makeRequest"
-import {loadStripe} from "@stripe/stripe-js"
+import {removeItem,resetCart} from '../../redux/cartReducer';
+import { makeRequest} from "../../makeRequest";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
-  const id = useParams().id
-  const products = useSelector(state =>state.cart.products);
 
-  const url = `/products/${id}?populate=*`;
-  const{data, loading, error} = useFetch(url) ;
+  const products = useSelector((state) => state.cart.products);  
 
   const dispatch = useDispatch();
 
   const subTotal = ()=>{
     let total = 0 ;
-    products.forEach(item => {
-      total += item.quantity * item.price
+    products.forEach((item) => {
+      total += item.quantity * item.price;
     });
-    return total.toFixed(2)
-  }
-  const stripePromise = loadStripe('pk_test_51MotQ2G3gUVndL3D1pZT7bX8Bt03ij0xuYDP7t3mAWvDQg3tBBQMyXzXlDao8GMDV7vReqX4bRHeYxraLpcZmxDU00I92RqHmD');
+    return total.toFixed(2);
+  };
+  const stripePromise = loadStripe(
+    'pk_test_51MotQ2G3gUVndL3D1pZT7bX8Bt03ij0xuYDP7t3mAWvDQg3tBBQMyXzXlDao8GMDV7vReqX4bRHeYxraLpcZmxDU00I92RqHmD'
+    );
 
-  const handlePayment = async ()=>{
+  const handlePayments = async () =>{
     try{
-
-    const stripe = await stripePromise
-
+    const stripe = await stripePromise;
     const res = await makeRequest.post('/orders',{
-      products
+      products,
     });
-
      await stripe.redirectToCheckout({
-       sessionId: res.data.sessionId,
+       sessionId: res.data.stripeSession.id,
      });
 
-    }catch(err){
-   console.log(err)
+    } catch(err){
+   console.log(err);
     }
   }
 
   return (
     <div className='cart'>
        <h1>Total items in your cart</h1>
-       {products.map((item) => (
+       {products?.map((item) => (
          <div className="items" key={item.id}>
             <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
                <div className="details">
                   <h1>{item.title}</h1>
-                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vel, veritatis voluptatum laborum, assumenda cum excepturi non ullam sunt possimus provident rerum. Animi neque, ullam sapiente distinctio quis asperiores ipsam.</p>
-                <div className="price">{item.quantity} * {item.price}</div>
+                     <p>{item.desc?.substring(0,100)}</p>
+                <div className="price">
+                  {item.quantity} x ${item.price}</div>
           </div>
 
-             <DeleteOutlinedIcon className='delete' onClick={()=> dispatch(removeItem(item.id))}/>
+             <DeleteOutlinedIcon
+              className='delete' 
+              onClick={()=> dispatch(removeItem(item.id))}/>
 
          </div>
          )
@@ -64,16 +61,16 @@ const Cart = () => {
        
      <div className="total">
        <span>SUBTOTAL</span>
-       <span>{subTotal()}</span>
+       <span>${subTotal()}</span>
      </div>
 
-     <button onClick={handlePayment}>CHECKOUT </button>
+     <button onClick={handlePayments}>CHECKOUT </button>
 
      <span className='reset'onClick={()=>dispatch(resetCart())}>Reset Cart</span>
 
 
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
