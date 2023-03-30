0ec04cf5063b1a930833d20f1f82e1a076da6190
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-31 00:19:51 +0300

    completed every feature

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -10,59 +10,58 @@ import {useSelector} from "react-redux";
 
 
 const Navbar = () => {
-   const[isopen, setIsOpen] = useState(false);
-   
+   const[isOpen, setIsOpen] = useState(false);
+
+
+ const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
    const products = useSelector(state =>state.cart.products);
 
   return (
-  <>
-  <div className="navbar">
-  <div className="wrapper">
 
-    <div className="left">
-
-  <Link className="link"to ="/">KIDDOSTORE</Link>
-
-    <div className="item">
-    <Link className="link"to ="/products/1">Women</Link>
-    <Link className="link"to ="/products/2">Men</Link>
-    <Link className="link" to ="/products/3">Children</Link>
- </div>
-
-</div>
-
-    <div className="right">
-
-      <div className="item">
-       <Link className="link"to ="/">Home</Link>
-    </div>
-      <div className="item">
-      <Link className="link"to ="/">About</Link>
-    </div>
-    <div className="item">
-      <Link className="link"to ="/">Contact</Link>
-    </div>
-    <div className="item">
-   <Link className="link"to ="/">Stores</Link>
-    </div>
-    <div className='icons'>
-      <SearchIcon />
-      <PersonOutlineIcon />
-      <FavoriteBorderOutlinedIcon />
-
-      <div className='cartIcon'onClick={()=>setIsOpen(!isopen)}>
-         <ShoppingCartOutlinedIcon />
-         <span>{products.length}</span>
-      </div>
-
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="/img/KIDDO_STOR.png" alt="" />
+            <Link className ="link" to="/" style={{fontWeight:600}}>KIDDOSTORE</Link>
+          </div>
+          
+          <div className="item">
+            <Link className ="link" to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/products/3">Children</Link>
+          </div>
         </div>
-
+        <div className="right">
+          <div className="item">
+            <Link className ="link" to="/">Home</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/">About</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/">Contact</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon style={{color:"black"}}/>
+            <PersonOutlineIcon style={{color:"black"}} />
+            <FavoriteBorderOutlinedIcon style={{color:"black"}}/>
+            <div className="cartIcon" onClick={()=>{handleToggle()}}>
+              <ShoppingCartOutlinedIcon style={{color:"black"}}/>
+              <span>{products.length}</span>
+            </div>
+          </div>
+        </div>
       </div>
-  </div> 
-  {isopen && <Cart/>}
-  </div>
- 
-  </>
+      {isOpen && <Cart/>}
+    </div> 
+  
   );
   };
          
