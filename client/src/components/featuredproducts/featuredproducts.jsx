c73f2a024cb91d4653f41ec64ac2e5583267e63f
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-30 17:05:34 +0300

    redo the top section

--- client/src/components/featuredproducts/featuredproducts.jsx
+++ client/src/components/featuredproducts/featuredproducts.jsx
@@ -1,24 +1,36 @@
 import useFetch from '../../hooks/useFetch';
 import "../../styles/featuredproducts.scss";
 import Card from '../Card/card';
+import Divider from '../Divider/divider';
 
 
 const Featuredproducts = ({type}) => {
  const{data, loading, error} = useFetch(`/products?populate=*&[filters][type][$eq]=${type}`)
 
   return (
-    <div className='featuredproducts'>
-         <div className="top">
-           <h1>{type}&nbsp;products</h1>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit quia, dicta eum ipsa non ullam consequatur harum quasi quis iusto asperiores consectetur dolorum incidunt recusandae molestias tempora esse? Repellat, temporibus.</p>
-
-         </div>
-         <div className="bottom">
-              {error? "Something's up!":
-              (loading? "loading" : data?.map((item) => <Card item={item} key={item.id}/>
-                ))}
-         </div>
+    <>
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas.
+        </p>
+      </div>
+      <div className="bottom">
+        {error
+          ? "Something's up!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
+      </div>
     </div>
+ 
+    </>
+  
   )
 }
 
