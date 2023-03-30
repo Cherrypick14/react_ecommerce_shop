7c3e20e5774d027236357461b40df47c066c0a2f
Cherrypick14 <cherrypicklet@gmail.com>
2023-03-31 00:20:39 +0300

    fixed the icons visibility

--- client/src/components/Slider/slider.jsx
+++ client/src/components/Slider/slider.jsx
@@ -16,37 +16,39 @@ const Slider = () => {
     setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
   };
 
-  // const textRef = useRef();
-  //   useEffect(()=>{
+  const textRef = useRef();
+    useEffect(()=>{
 
-  //   init(textRef.current,{
+    init(textRef.current,{
 
-  //        backDelay:1500,
-  //        showCursor:true,
-  //        strings:[
-  //            'Welcome',
-  //            'to ',
-  //            'KiddoStore'
-  //        ]
-  //   })
+         backDelay:1500,
+         showCursor:true,
+         strings:[
+             'Welcome',
+             'to ',
+             'KiddoStore'
+         ]
+    })
 
-  //   }, [])
+    }, [])
   const data = [
     "https://images.pexels.com/photos/4132651/pexels-photo-4132651.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/1857000/pexels-photo-1857000.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/2881785/pexels-photo-2881785.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
   ];
 
-
-
   return (
    <div className="slider">
-      <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
-      {/* <h1 ref ={textRef}></h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto eius voluptatibus sint est possimus? Atque totam voluptatem veritatis. Qui laborum veritatis ipsameos tenetur facilis quo minima reprehenderit aperiam reiciendis?</p> */}
+      <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`,Position:"relative"}}>
+        <div className="mystyletext" style={{position:"absolute"}}>
+        <h1 ref ={textRef}></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto eius voluptatibus sint est possimus? Atque totam voluptatem veritatis. Qui laborum veritatis ipsameos tenetur facilis quo minima reprehenderit aperiam reiciendis?</p>
+        </div>
+         
         <img src={data[0]} alt="" />
         <img src={data[1]} alt="" />
         <img src={data[2]} alt="" />
+        <div className="overlay"></div>
       </div>
       <div className="icons">
         <div className="icon" onClick={prevSlide}>
